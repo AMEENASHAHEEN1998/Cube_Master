6e3066247d9514375c2823ebc8aa9a2dcb6ce0e4
AMEENASHAHEEN1998 <ameena.sh.98@gmail.com>
2022-01-12 03:27:29 +0200

    latest

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,21 +15,5 @@ class UserController extends Controller
         return view('user', compact('users'));
     }
 
-    public function calculate(Request $request){
-        $Amount_Commission = $request->Amount_Commission;
-        $Discount = $request->Discount;
-        $Rate_VAT = $request->Rate_VAT;
-       
-        $Amount_Commission2 = $Amount_Commission - $Discount;
 
-        if ($Amount_Commission === 'undefined' || !$Amount_Commission) {
-            session()->flash('يرجي ادخال مبلغ العمولة ');
-        } else {
-            $intResults = $Amount_Commission2 * $Rate_VAT / 100;
-            $intResults2 = floatval($intResults + $Amount_Commission2);
-            $data['sumq'] = floatval($intResults);
-            $data['sumt'] = floatval($intResults2);
-            return response()->json($data);
-        }
-    }
 }
